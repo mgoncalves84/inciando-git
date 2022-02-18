3484b7b4f1429ae5b7e0b930836b240568a674aa
mgoncalves84 <celogoncalves84@gmail.com>
2022-02-18 13:01:34 -0300

    adicionando primeiro programa

---
+++ olamundo.js
@@ -0,0 +1,16 @@
+//comentario 1 var/for/if/ console.log
+var texto = "olamundo"
+var texto2 = "gabigol"
+console.log(texto + texto2)
+var cinco = 6
+console.log(cinco + 10)
+for (let i = 0; i < 9; i++) {
+    console.log(texto2 + " " + i)
+    i=2
+
+
+    console.log(texto2 + " " + i)
+    if(i==2){
+        console.log("hernane é melhor que messi")
+    }
+}
