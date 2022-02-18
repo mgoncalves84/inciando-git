bbdf63ace2dd616c84c10e5ec909e97068228d0d
Ruben Bustos <ruben.bustos.gonzalez@everis.com>
2022-02-18 16:09:35 +0000

    Segundo commit

--- olamundo.js
+++ olamundo.js
@@ -1,15 +1,19 @@
-//comentario 1 var/for/if/ console.log
-var texto = "olamundo"
+//comentario var/for/if/ console.log
+
+var texto = "Olá mundo"
 var texto2 = "gabigol"
-console.log(texto + texto2)
-var cinco = 6
+
+// log de texto (String) concatenados
+console.log(texto + " " + texto2)
+var cinco = 5
+
+// log de operações matemáticas
 console.log(cinco + 10)
+
+//
 for (let i = 0; i < 9; i++) {
     console.log(texto2 + " " + i)
-    i=2
-
-
-    console.log(texto2 + " " + i)
+    // if condicional
     if(i==2){
         console.log("hernane é melhor que messi")
     }
